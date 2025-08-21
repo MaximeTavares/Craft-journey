ff72752e057d6e74a8fa852d469da5d0bc5ed064
Maxime Tavares <t.maxime59@gmail.com>
2025-08-21 14:24:54 +0200

    Kit added with a list of blocks and keywords

---
+++ app/src/main/java/com/craftjourney/Kit.java
@@ -0,0 +1,30 @@
+package com.craftjourney;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Kit {
+
+    private Set<IBloc> blocs = new LinkedHashSet<IBloc>();
+    private Set<String> keywords = new LinkedHashSet<String>();
+
+    public Kit() {
+        // Initialize the kit with some default blocs
+        blocs.add(new Wall(3, 2, 2, true));
+        blocs.add(new Wall(3, 2, 2, true));
+        blocs.add(new Wall(2, 1, 2, false));
+        blocs.add(new Wall(2, 1, 2, false));
+        blocs.add(new Door(1, 1, 2, true));
+
+        keywords.add("Cabane");
+        keywords.add("Muraille");
+    }
+
+    public void displayKit() {
+        System.out.println("Kit bloc's number: " + blocs.size());
+        System.out.println("Kit's keywords': ");
+        for (String keyword : keywords) {
+            System.out.println(keyword);
+        }
+    }
+}
--- app/src/main/java/com/craftjourney/Main.java
+++ app/src/main/java/com/craftjourney/Main.java
@@ -3,17 +3,8 @@ package com.craftjourney;
 public class Main {
     public static void main(String[] args) {
 
-        Wall wall = new Wall(10, 5, 3, true);
-        wall.displayDescription();
-        wall.displayInfo();
-
-        System.out.println("Is the wall passable? " + wall.isPassable());
-
-        Door door = new Door(2, 1, 3, false);
-        door.displayDescription();
-        door.displayInfo();
-
-        System.out.println("Is the door locked? " + door.isLocked());
+        Kit startingKit = new Kit();
+        startingKit.displayKit();
 
     }
 }
